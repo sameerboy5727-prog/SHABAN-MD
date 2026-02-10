0ae3187445dec0bc3e1723ef36795053555a7751
dilawariam2-byte <dilawariam2@gmail.com>
2026-02-10 14:13:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const staticConfig = {
     // ==================== BAILEYS & SESSION ====================
     BAILEYS: "@whiskeysockets/baileys",
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "SHABAN~v2cW",
     CDN: process.env.CDN || "https://bandaheali-cdn.koyeb.app",
 };
 
